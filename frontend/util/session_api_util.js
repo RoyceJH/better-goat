bb8c2726b1d2125992c5117749ded27adcef8a41
royce <kim.royce2@gmail.com>
2017-04-18 17:15:53 -0400

    add session api util

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,22 @@
+export const login = (user) => {
+  return $.ajax({
+    method:'POST',
+    url: '/api/session',
+    data: { user }
+  });
+};
+
+export const logout = () => {
+  return $.ajax({
+    method:'DELETE',
+    url: '/api/session'
+  });
+};
+
+export const signup = (user) => {
+  return $.ajax({
+    method:'POST',
+    url: '/api/user',
+    data: { user}
+  });
+};
