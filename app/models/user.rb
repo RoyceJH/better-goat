33fc267e60c35f2a35ac07afb172314a601ee224
royce <kim.royce2@gmail.com>
2017-04-19 15:15:57 -0400

    add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true;
   validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
   attr_reader :password
