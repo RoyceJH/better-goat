14f47a1c2e5ce97a22336f0b21ab1c9a22c5eeec
royce <kim.royce2@gmail.com>
2017-04-19 19:40:41 -0400

    add note skeleton

---
+++ frontend/components/note/new_note.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class NewNote extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='new-note'>
+        NEw Note
+      </div>
+
+    );
+  }
+}
+
+export default NewNote;
