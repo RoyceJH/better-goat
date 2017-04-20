f5edf5db2663b67c99be2aa807705d3c9805d0da
royce <kim.royce2@gmail.com>
2017-04-20 09:24:54 -0400

    add css live reload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # For live css reloading
+  config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
 end
