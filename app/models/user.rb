a31390029ab1a06723635e770bc7359fac8c8e1f
royce <kim.royce2@gmail.com>
2017-04-19 21:11:12 -0400

    add notebook association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
-  validates :username, uniqueness: true;
+  validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
   attr_reader :password
+
+  has_many :notebooks,
+           primary_key: :id,
+           foreign_key: :author_id,
+           class_name: :Notebook
 
   def self.find_by_credentials(username, password)
     user = User.find_by_username(username)
