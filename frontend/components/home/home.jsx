6d17fc0947834d28a03668092f17dbbfc244c427
royce <kim.royce2@gmail.com>
2017-04-23 22:17:36 -0400

    add modal pieces

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HomeSideBarContainer from './home_side_bar_container';
 import NotebookIndexContainer from '../notebook/notebook_index_container';
 import NoteEditorContainer from '../notes/note_editor_container';
+import RootModalContainer from '../modal/root_modal_container';
 
 class Home extends React.Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class Home extends React.Component {
         <NotebookIndexContainer />
         {this.props.children}
         <NoteEditorContainer />
+        <RootModalContainer />
       </div>
     );
   }
