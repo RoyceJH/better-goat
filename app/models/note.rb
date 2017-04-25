cd8f0ba32334da37d2330f36400010cc93a02954
royce <kim.royce2@gmail.com>
2017-04-25 13:47:16 -0400

    add column for note preview

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,12 +9,17 @@
 #  body        :text
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  preview     :string           not null
 #
 
 class Note < ActiveRecord::Base
-  validates :title, :author, :notebook, presence: true
+  validates :title, :author, :notebook, :preview, presence: true
 
-  belongs_to :notebook
+  belongs_to :notebook,
+             primary_key: :id,
+             foreign_key: :notebook_id,
+             class_name: :Notebook
+
   belongs_to :author,
              primary_key: :id,
              foreign_key: :author_id,
