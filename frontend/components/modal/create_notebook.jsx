3cd305b1e05e14fc70c3fc2dc152a0a8a21a6c63
royce <kim.royce2@gmail.com>
2017-05-09 11:26:02 -0400

    refactor using shared form

--- frontend/components/modal/create_notebook.jsx
+++ frontend/components/modal/create_notebook.jsx
@@ -3,80 +3,23 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
 import { createNotebook } from '../../actions/notebook_actions';
-import { removeModal } from '../../actions/modal_actions';
-import { removeSlideout } from '../../actions/slideout_actions';
-
-const mapStateToProps = (state, ownProps) => ({
-
-});
+import CreateForm from './create_form';
 
 const mapDispatchToProps = dispatch => ({
   createNotebook: (notebook) => dispatch(createNotebook(notebook)),
-  removeModal: () => dispatch(removeModal()),
-  removeSlideout: () => dispatch(removeSlideout()),
 });
 
 class CreateNotebook extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {title: ""};
-    this.handleChange = this.handleChange.bind(this);
-    this.submitNotebook = this.submitNotebook.bind(this);
-    this.cancelCreate = this.cancelCreate.bind(this);
-  }
-
-  submitNotebook(e) {
-    const newNotebook = {title: this.state.title};
-    this.props.removeModal();
-    this.props.removeSlideout();
-    this.props.createNotebook(newNotebook).then((action) => this.props.router.push(`/home/notebook/${action.notebook.id}`));
-  }
-
-  cancelCreate(e) {
-    this.props.removeModal();
-  }
-
-  handleChange(e) {
-    let title = e.target.value;
-    this.setState({title});
-  }
-
   render() {
-    const submitClass = this.state.title === '' ? 'inactive' : '';
-    const submitAction = this.state.title === '' ? '' : this.submitNotebook;
     return(
-      <div className='modal-create-notebook'>
-        <div className='modal-create-notebook-header'>
-          <i className="fa fa-file-text-o" aria-hidden="true"></i>
-          <label>CREATE NOTEBOOK</label>
-          <div className='modal-create-notebook bottom-line'/>
-        </div>
-
-        <input
-          type='text'
-          className='create-notebook-title'
-          onChange={this.handleChange}
-          placeholder='Title your notebook'
-          value={this.state.title}
-          />
-
-        <div className='notebook-modal'>
-          <input
-            className='cancel'
-            onClick={this.cancelCreate}
-            type='submit'
-            value='Cancel'
-            />
-          <input
-            className={`submit ${submitClass}`}
-            onClick={submitAction}
-            type='submit'
-            value='Create notebook'
-            />
-        </div>
-      </div>
+      <CreateForm
+        createAction={this.props.createNotebook}
+        item={'notebook'}
+        reroute={(action) => this.props.router.push(`/home/notebook/${action.notebook.id}`)}
+        placeholder='Title your notebook'
+      />
     );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(CreateNotebook));
+export default connect(null, mapDispatchToProps)(withRouter(CreateNotebook));
