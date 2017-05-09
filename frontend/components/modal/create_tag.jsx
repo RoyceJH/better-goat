5c13a0e46011a0fdf10b60469858996de07e3e95
royce <kim.royce2@gmail.com>
2017-05-09 11:15:57 -0400

    refactor create form

--- frontend/components/modal/create_tag.jsx
+++ frontend/components/modal/create_tag.jsx
@@ -3,80 +3,23 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
 import { createTag } from '../../actions/tag_actions';
-import { removeModal } from '../../actions/modal_actions';
-import { removeSlideout } from '../../actions/slideout_actions';
-
-const mapStateToProps = (state, ownProps) => ({
-
-});
+import CreateForm from './create_form';
 
 const mapDispatchToProps = dispatch => ({
   createTag: (tag) => dispatch(createTag(tag)),
-  removeModal: () => dispatch(removeModal()),
-  removeSlideout: () => dispatch(removeSlideout()),
 });
 
 class CreateTag extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {title: ""};
-    this.handleChange = this.handleChange.bind(this);
-    this.submitNotebook = this.submitNotebook.bind(this);
-    this.cancelCreate = this.cancelCreate.bind(this);
-  }
-
-  submitNotebook(e) {
-    const newTag = {title: this.state.title};
-    this.props.removeModal();
-    this.props.removeSlideout();
-    this.props.createTag(newTag).then((action) => this.props.router.push(`/home/tag/${action.tag.id}`));
-  }
-
-  cancelCreate(e) {
-    this.props.removeModal();
-  }
-
-  handleChange(e) {
-    let title = e.target.value;
-    this.setState({title});
-  }
-
   render() {
-    const submitClass = this.state.title === '' ? 'inactive' : '';
-    const submitAction = this.state.title === '' ? '' : this.submitNotebook;
     return(
-      <div className='modal-create-notebook'>
-        <div className='modal-create-notebook-header'>
-          <i className="fa fa-file-text-o" aria-hidden="true"></i>
-          <label>CREATE TAG</label>
-          <div className='modal-create-notebook bottom-line'/>
-        </div>
-
-        <input
-          type='text'
-          className='create-notebook-title'
-          onChange={this.handleChange}
-          placeholder='Title your tag'
-          value={this.state.title}
-          />
-
-        <div className='notebook-modal'>
-          <input
-            className='cancel'
-            onClick={this.cancelCreate}
-            type='submit'
-            value='Cancel'
-            />
-          <input
-            className={`submit ${submitClass}`}
-            onClick={submitAction}
-            type='submit'
-            value='Create tag'
-            />
-        </div>
-      </div>
+      <CreateForm
+        createAction={this.props.createTag}
+        item={'tag'}
+        reroute={(action) => {this.props.router.push(`/home/tags/${action.tag.id}`);}}
+        placeholder='Name your tag'
+      />
     );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(CreateTag));
+export default connect(null, mapDispatchToProps)(withRouter(CreateTag));
