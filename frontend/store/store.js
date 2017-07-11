80fff7d63e5288960ba3a01358f0d074f54995b8
royce <kim.royce2@gmail.com>
2017-07-11 13:37:43 -0400

    remove comment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,7 +5,6 @@ import thunk from 'redux-thunk';
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
   const { createLogger } = require('redux-logger');
   middlewares.push(createLogger());
 }
